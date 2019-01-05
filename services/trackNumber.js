812cd8a7a8b36eaa8e1112bf0bcff5f12a73da5a
Omer Dogan <omerdogan3@gmail.com>
2019-01-05 10:29:04 +0300

    Update trackNumber.js

--- services/trackNumber.js
+++ services/trackNumber.js
@@ -11,12 +11,12 @@ module.exports = (app) => {
 
       //userid
       //phonenumber
-      let phoneNumber = req.body.phoneNumber.replace(/[^+0-9]/g, '').replace(/^00/, '');
-
+      let phoneNumber = req.body.phoneNumber.replace(/[^+0-9]/g, '').replace(/^00/, '').replace(/\+/g, '');
+      
       dbo.collection("users").updateOne({_id: ObjectId(req.body.userid)}, {
         $addToSet: {trackedNumbers: {phoneNumber, username: req.body.username}}
       },function(err,result){
-        dbo.collection("trackedUsers").insertOne({_id: req.body.phoneNumber, username: req.body.username, userData: []}, {upsert: true}, function(err,result){
+        dbo.collection("trackedUsers").insertOne({_id: phoneNumber, username: req.body.username, userData: []}, {upsert: true}, function(err,result){
           res.send({
             userid: req.body.userid,
             phoneNumber: phoneNumber,
